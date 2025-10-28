9478a5544f4ac29a1140ca2fdcac46d4aafd0cb7
ali2200 <aliabdalla2200@gmail.com>
2025-10-28 10:35:02 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,5 +11,4 @@ const nextConfig = {
     unoptimized: true,
   },
 }
-
 export default nextConfig
